0d9b089334638d56dca05aba79a1d277004365c8
kutty023 <arthiaaru23@gmail.com>
2025-03-09 07:16:22 +0530

    navbar styling is completed

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css';
 import Navbar from './components/navbar.jsx';
-import Home from './components/home.jsx';
-import Service from './components/service.jsx';
-import Contact from './components/contact.jsx';
-import Agency from './components/agency.jsx';
-import CaseStudy from './components/caseStudey.jsx';
-import Resource from './components/resource.jsx';
+// import Home from './components/home.jsx';
+// import Service from './components/service.jsx';
+// import Contact from './components/contact.jsx';
+// import Agency from './components/agency.jsx';
+// import CaseStudy from './components/caseStudey.jsx';
+// import Resource from './components/resource.jsx';
 
 function App() {
   return (
     <>
     <Navbar />
-    <Home />
+    {/* <Home />
     <Service />
     <Agency />
     <CaseStudy />
     <Resource />
-    <Contact />
+    <Contact /> */}
     </>
   )
 }
